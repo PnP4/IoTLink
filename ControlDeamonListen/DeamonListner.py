9f4423ba6c1a4ade79cc1baa8aa4415f34e9e14f
NickHub4017 <nrv.rox.u@gmail.com>
2016-08-31 19:06:16 +0530

    Add Next Node Connection Function.

--- ControlDeamonListen/DeamonListner.py
+++ ControlDeamonListen/DeamonListner.py
@@ -1,6 +1,6 @@
 import socket
 from ConfigFile.ConfigParser import ConfigMonitor
-
+from Utils.NextNode import NextNode
 
 
 cm=ConfigMonitor("/home/nrv/PycharmProjects/PnpGlobalLink/ConfigFile/config.xml")
@@ -10,8 +10,8 @@ ListenIP=cm.getIp();
 ListenPORT=int(cm.getPort())
 BufferSize=1024
 
-print cm.getNextNodeList()
-exit(0)
+
+
 
 serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -21,6 +21,16 @@ serversocket.listen(1)  #Currenly listen to one
 
 reply=" This Reply"
 
+def conectToNextNode(NextNode):
+    clientsocket = socket.socket()
+    clientsocket.connect(NextNode.getConnectableObject())
+    clientsocket.send("--------")
+    msg=clientsocket.recv(1024)
+    print msg
+    clientsocket.close()
+
+
+
 while True:
     clientsock,clientaddr=serversocket.accept()
     print "Connection establish with client:- ", clientaddr
@@ -29,8 +39,17 @@ while True:
         clientdata=clientsock.recv(BufferSize)
         if(not clientsock):
             break
-        clientsock.send(reply);
+        for node in cm.getNextNodeList():
+            conectToNextNode(node)
+        clientsock.send(reply)
         print len(reply)
         clientsock.close()
 
 
+def conectToNextNode(NextNode):
+    clientsocket = socket.socket()
+    clientsocket.connect(NextNode.getConnectableObject())
+    clientsocket.send("--------")
+    msg=clientsocket.recv(1024)
+    print msg
+    clientsocket.close()
