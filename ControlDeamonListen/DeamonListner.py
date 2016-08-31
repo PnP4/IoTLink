b4df15829612281b82fcc447fea83b1a8e5f6b17
NickHub4017 <nrv.rox.u@gmail.com>
2016-08-31 21:56:44 +0530

    Refactor ControlDeamon to In a Class

--- ControlDeamonListen/DeamonListner.py
+++ ControlDeamonListen/DeamonListner.py
@@ -1,55 +1,69 @@
 import socket
 from ConfigFile.ConfigParser import ConfigMonitor
-from Utils.NextNode import NextNode
+
+class ControlDeamon:
+    def __init__(self):
+        self.cm = ConfigMonitor("/home/nrv/PycharmProjects/PnpGlobalLink/ConfigFile/config.xml")
+        self.ListenIP = self.cm.getIp();
+        self.ListenPORT = int(self.cm.getPort())
+        self.BufferSize = 1024
+
+    def connect(self):
+        try:
+            self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.serversocket.bind((self.ListenIP, self.ListenPORT))
+            self.serversocket.listen(1)  # Currenly listen to one
+            return True
+        except:
+            return False
 
 
-cm=ConfigMonitor("/home/nrv/PycharmProjects/PnpGlobalLink/ConfigFile/config.xml")
+    def handleClient(self):
+        reply="********"
+        while True:
+            clientsock, clientaddr = self.serversocket.accept()
+            print "Connection establish with client:- ", clientaddr
+
+            while True:
+                clientdata = clientsock.recv(self.BufferSize)
+                if (not clientsock):
+                    break
+                for node in self.cm.getNextNodeList():
+                    self.conectToNextNode(node)
+                clientsock.send(reply)
+                print len(reply)
+                clientsock.close()
+
+    def conectToNextNode(self,NextNode):
+        try:
+            clientsocket = socket.socket()
+            clientsocket.connect(NextNode.getConnectableObject())
+            clientsocket.send("--------")
+            msg = clientsocket.recv(1024)
+            print msg
+            clientsocket.close()
+            return True
+        except:
+            return False
 
 
-ListenIP=cm.getIp();
-ListenPORT=int(cm.getPort())
-BufferSize=1024
+
+c=ControlDeamon()
+c.connect()
+c.handleClient()
 
 
 
 
-serversocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-serversocket.bind((ListenIP,ListenPORT))
-
-serversocket.listen(1)  #Currenly listen to one
-
-reply=" This Reply"
-
-def conectToNextNode(NextNode):
-    clientsocket = socket.socket()
-    clientsocket.connect(NextNode.getConnectableObject())
-    clientsocket.send("--------")
-    msg=clientsocket.recv(1024)
-    print msg
-    clientsocket.close()
 
 
 
-while True:
-    clientsock,clientaddr=serversocket.accept()
-    print "Connection establish with client:- ", clientaddr
-
-    while True:
-        clientdata=clientsock.recv(BufferSize)
-        if(not clientsock):
-            break
-        for node in cm.getNextNodeList():
-            conectToNextNode(node)
-        clientsock.send(reply)
-        print len(reply)
-        clientsock.close()
 
 
-def conectToNextNode(NextNode):
-    clientsocket = socket.socket()
-    clientsocket.connect(NextNode.getConnectableObject())
-    clientsocket.send("--------")
-    msg=clientsocket.recv(1024)
-    print msg
-    clientsocket.close()
+
+
+
+
+
+
+
