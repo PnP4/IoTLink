8ba8ee8e0ea1498c1541291e268aad7b76f30f19
NickHub4017 <nrv.rox.u@gmail.com>
2016-09-01 19:33:00 +0530

    Change the handleClient function untill  json id fully receved.

--- ControlDeamonListen/DeamonListner.py
+++ ControlDeamonListen/DeamonListner.py
@@ -1,36 +1,45 @@
+import json
 import socket
 from ConfigFile.ConfigParser import ConfigMonitor
 import netifaces
 
 class ControlDeamon:
-    def __init__(self):
+    def __init__(self):  #At initilasisation phase it will read the config and config itself
         self.cm = ConfigMonitor("/home/nrv/PycharmProjects/PnpGlobalLink/ConfigFile/config.xml")
         self.ListenIP = self.cm.getIp();
         self.ListenPORT = int(self.cm.getPort())
         self.BufferSize = 1024
-        self.myips = self.getAllInterfaceAddresses()  #get the idea of the network address of the node has.
+        self.myips = self.getAllInterfaceAddresses()
 
 
-    def getAllInterfaceAddresses(self):
+    def getAllInterfaceAddresses(self):  # get the idea of the network address of the node has.
         iplist=[]
         interface_list = netifaces.interfaces()
         for iname in interface_list:
-            if (("eth" in iname) or ("wlan" in iname)):
+            if (("eth" in iname) or ("wlan" in iname)): #get only eth and wlan (ethernet and wifi addresses)
                 try:
-                    internetIps = netifaces.ifaddresses(iname)[netifaces.AF_INET]
+                    internetIps = netifaces.ifaddresses(iname)[netifaces.AF_INET]  #fetch only IPv4 address only
                     iplist.append(internetIps[0]["addr"])
                 except:
                     pass
         return iplist
+
+    def filetrOutSelfIps(self,node):
+
+        if(node.getip() in self.myips):
+            return True
+        if("127.0." in node.getip()):
+            return True
+        return False
 
     def connect(self):
         try:
             self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.serversocket.bind((self.ListenIP, self.ListenPORT))
             self.serversocket.listen(1)  # Currenly listen to one
-
             return True
-        except:
+        except Exception as e:
+            print e,"DemonListner @connect()"
             return False
 
     def handleClient(self):
@@ -38,16 +47,25 @@ class ControlDeamon:
         while True:
             clientsock, clientaddr = self.serversocket.accept()
             print "Connection establish with client:- ", clientaddr
-
-            while True:
+            totalclientdata=""
+            while True:  # Will read the pipe untill no of { and no of } matches. [Parserble]
                 clientdata = clientsock.recv(self.BufferSize)
-                if (not clientsock):
+                totalclientdata=totalclientdata+clientdata
+                try:
+                    json.loads(totalclientdata)  #Json is parsable means json is ccompletely receved
                     break
-                for node in self.cm.getNextNodeList():
+                except Exception as e:
+                    print e," while capturing the json commnd string"
+                    continue
+            for node in self.cm.getNextNodeList():
+                if(not self.filetrOutSelfIps(node)):
+                    print "Done"
                     self.conectToNextNode(node)
-                clientsock.send(reply)
-                print len(reply)
-                clientsock.close()
+                else:
+                    print "Loop detected"
+            clientsock.send(reply)
+            print len(reply)
+            clientsock.close()
 
     def conectToNextNode(self,NextNode):
         try:
@@ -59,5 +77,5 @@ class ControlDeamon:
 
 
 c=ControlDeamon()
-#c.connect()
-#c.handleClient()
+if(c.connect()):
+    c.handleClient()
