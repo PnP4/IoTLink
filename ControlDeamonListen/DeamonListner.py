65009b6e26b922c5e518fa7ec22e242e775b8327
NickHub4017 <nrv.rox.u@gmail.com>
2016-09-01 19:02:12 +0530

    Add self network address  discovering function. Add ReadME

--- ControlDeamonListen/DeamonListner.py
+++ ControlDeamonListen/DeamonListner.py
@@ -1,5 +1,6 @@
 import socket
 from ConfigFile.ConfigParser import ConfigMonitor
+import netifaces
 
 class ControlDeamon:
     def __init__(self):
@@ -7,12 +8,27 @@ class ControlDeamon:
         self.ListenIP = self.cm.getIp();
         self.ListenPORT = int(self.cm.getPort())
         self.BufferSize = 1024
+        self.myips = self.getAllInterfaceAddresses()  #get the idea of the network address of the node has.
+
+
+    def getAllInterfaceAddresses(self):
+        iplist=[]
+        interface_list = netifaces.interfaces()
+        for iname in interface_list:
+            if (("eth" in iname) or ("wlan" in iname)):
+                try:
+                    internetIps = netifaces.ifaddresses(iname)[netifaces.AF_INET]
+                    iplist.append(internetIps[0]["addr"])
+                except:
+                    pass
+        return iplist
 
     def connect(self):
         try:
             self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.serversocket.bind((self.ListenIP, self.ListenPORT))
             self.serversocket.listen(1)  # Currenly listen to one
+
             return True
         except:
             return False
@@ -43,5 +59,5 @@ class ControlDeamon:
 
 
 c=ControlDeamon()
-c.connect()
-c.handleClient()
+#c.connect()
+#c.handleClient()
