44f886e054dd2a093f409a9ee7e266d15a6df425
NickHub4017 <nrv.rox.u@gmail.com>
2016-09-17 15:08:45 -0400

    Add Table create query

--- DB/DataBase.py
+++ DB/DataBase.py
@@ -4,8 +4,11 @@ conn = sqlite3.connect('globalpnp.db')
 
 cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE netmetadata(ip text, port int,type text)''')
+cursor.execute('''CREATE TABLE netmetadata(name text,ip text, port int,type text)''')
 
+cursor.execute("Insert INTO netmetadata(name) values('inp')")
+cursor.execute("Insert INTO netmetadata(name) values('out')")
+cursor.execute("Insert INTO netmetadata(name) values('cont')")
 
 conn.commit()
 conn.close()
