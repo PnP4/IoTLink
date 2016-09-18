6de8a2821d577f8cefed7eda4977c3c0d0d09728
NickHub4017 <nrv.rox.u@gmail.com>
2016-09-18 19:15:04 -0400

    Add Table Init Query

--- DB/DataBase.py
+++ DB/DataBase.py
@@ -1,14 +1,43 @@
 import sqlite3
 
-conn = sqlite3.connect('globalpnp.db')
 
-cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE netmetadata(name text,ip text, port int,type text)''')
+class SQLDB:
+    def __init__(self):
+        try:
+            self.getCon()
+        except:
+            pass
 
-cursor.execute("Insert INTO netmetadata(name) values('inp')")
-cursor.execute("Insert INTO netmetadata(name) values('out')")
-cursor.execute("Insert INTO netmetadata(name) values('cont')")
+        try:
+            self.CreateTable()
+        except:
+            pass
 
-conn.commit()
-conn.close()
+        try:
+            self.insertInitData()
+        except:
+            pass
+
+    def getCon(self):
+        self.dbconnection = sqlite3.connect('globalpnp.db')
+
+    def CreateTable(self):
+        cursor = self.dbconnection.cursor()
+        cursor.execute('''CREATE TABLE netmetadata(name text,ip text, port int,type text)''')
+        self.dbconnection.commit()
+        self.dbconnection.close()
+
+    def insertInitData(self):
+        self.getCon()
+        cursor = self.dbconnection.cursor()
+        cursor.execute("Insert INTO netmetadata(name) values('inp')")
+        cursor.execute("Insert INTO netmetadata(name) values('out')")
+        cursor.execute("Insert INTO netmetadata(name) values('cont')")
+        self.dbconnection.commit()
+        self.dbconnection.close()
+
+
+
+
+
